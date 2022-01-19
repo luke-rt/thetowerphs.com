425daf7dfe6208270bbd97ba6fdf21ba1290a9fa
ltgr <luke.rtong@gmail.com>
2022-01-18 22:20:17 -0500

    rudimentary loader data usage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,23 @@
-import { getPostsTest } from "../utils/queries";
+import { Link, useLoaderData } from "remix";
+import { article } from "@prisma/client";
+import { getDataTest } from "../lib/queries";
 
 export let loader = () => {
-  return getPostsTest();
+  return getDataTest();
 }
 
 export default function Index() {
+  let articles: article[] = useLoaderData();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
+      <ul>
+        {articles.map(article => (
+            <li key={article.id}>
+                <Link to={article.id}>{article.title}</Link>
+            </li>
+        ))}
+      </ul>
     </div>
   );
 }
