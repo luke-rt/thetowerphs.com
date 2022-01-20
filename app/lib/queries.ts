ba3d9ac8dfa9b3be4679d451d289d9e500f84012
ltgr <luke.rtong@gmail.com>
2022-01-20 12:56:05 -0500

    Article preview and dedicated page

--- app/lib/queries.ts
+++ app/lib/queries.ts
@@ -2,13 +2,45 @@ import { article, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function getDataTest() {
+export async function getArticles() {
   await prisma.$connect();
-  const articles: article[] = await prisma.article.findMany({
-    where: { published: true },
-  })
+
+  const articles: article[] = await prisma.article.findMany();
 
   prisma.$disconnect();
+
   console.log(articles)
   return articles;
+}
+
+export async function getArticleById(id: string) {
+  await prisma.$connect();
+
+  const article = await prisma.article.findFirst({
+    where: {
+      id: id,
+      published: true,
+    },
+  })
+  prisma.$disconnect();
+
+  return article;
+}
+
+export async function getArticlesByCategory(cat: string) {
+  await prisma.$connect();
+
+  const articles = await prisma.article.findMany({
+    where: {
+      section: cat,
+      published: true,
+    },
+  })
+  prisma.$disconnect();
+
+  return articles;
+}
+
+export async function expandCategoryName(cat: string) {
+
 }
---
+++ app/routes/articles.$slug.tsx
@@ -0,0 +1,22 @@
+import { LoaderFunction, useLoaderData } from "remix";
+import { article } from "@prisma/client";
+import invariant from 'tiny-invariant';
+
+import { getArticleById } from "~/lib/queries";
+
+export let loader: LoaderFunction = async({params}) => {
+  invariant(params.slug, "expected params.slug");
+
+  return getArticleById(params.slug);
+}
+
+export default function Article() {
+  let article: article = useLoaderData();
+  return (
+    <div className="article">
+      <h1 className="title">{article.title}</h1>
+
+    </div>
+
+  );
+}
