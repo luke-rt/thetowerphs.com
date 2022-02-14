d238f4d1451fc1b12e188d276cabd712c462a681
ltgr <luke.rtong@gmail.com>
2022-02-13 19:20:42 -0500

    Added images and default banner

--- app/components/Preview.tsx
+++ app/components/Preview.tsx
@@ -12,24 +12,31 @@ export default function Preview({article, category}: Props) {
 	sentences.pop();
 	const preview = sentences.join(" ").replace(/[\n\r\t\s]+/g, " ") + "...";
 
+	if(!article.img) article.img = "/assets/default.png";
+
 	return(
 		<div className="articlePreview">
-			<div className="category">
-				{category &&
-           <Link to={ "/category/" + article.category }>{expandCategorySlug(article.category)}</Link>
-				}
+			<div className="left">
+				<img src={article.img} alt="Article preview"/>
 			</div>
-			<div>
-				<Link to={ "/articles/" + article.year + "/" + article.month + "/" + article.category + "/" + encodeURI(article.title) } className="title">{article.title}</Link>
-				<span>{ displayDate(article.year, article.month) }</span>
-			</div>
-			<div className="authors">
-				{article.authors.map((author, index) => (
-					<Link key={index} to={ "/credit/" + encodeURI(author) } >{author}</Link>
-				))}
-			</div>
-			<div className="preview">
-				{preview}
+			<div className="right">
+				<div className="category">
+					{category &&
+						<Link to={ "/category/" + article.category }>{expandCategorySlug(article.category)}</Link>
+					}
+				</div>
+				<div>
+					<Link to={ "/articles/" + article.year + "/" + article.month + "/" + article.category + "/" + encodeURI(article.title) } className="title">{article.title}</Link>
+					<span>{ displayDate(article.year, article.month) }</span>
+				</div>
+				<div className="authors">
+					{article.authors.map((author, index) => (
+						<Link key={index} to={ "/credit/" + encodeURI(author) } >{author}</Link>
+					))}
+				</div>
+				<div className="preview-text">
+					{preview}
+				</div>
 			</div>
 		</div>
 	);
