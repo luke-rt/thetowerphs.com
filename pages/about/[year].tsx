76f8aca085388b66b72a6e756093b8a2142fead7
Luke T <56455367+luketio@users.noreply.github.com>
2022-03-26 21:59:41 -0400

    Update [year].tsx

---
+++ pages/about/[year].tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Subscribe() {
+	return(
+		<div>
+			<Head>
+				<title>About | The Tower</title>
+				<meta
+					property="og:title"
+					content="About | The Tower"
+				/>
+				<meta
+					property="og:description"
+					content="About the Tower"
+				/>
+			</Head>
+			<h1>About</h1>
+			
+		</div>
+	);
+}
