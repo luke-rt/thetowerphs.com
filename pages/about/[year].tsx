8d25c0db1a04cb6514a656b6edf2d4249e4976d4
ltgr <luke.rtong@gmail.com>
2022-03-26 22:10:02 -0400

    Fixed about page

--- pages/about/[year].tsx
+++ pages/about/[year].tsx
@@ -1,10 +1,29 @@
 import Head from "next/head";
+import styles from "~/styles/about.module.scss";
 
-export default function Subscribe() {
+interface Props {
+	year: number,
+}
+
+interface Params {
+	params: {
+		year: number,
+	}
+}
+
+export function getServerSideProps({ params }: Params) {
+	return {
+		props: {
+			year: params.year,
+		},
+	};
+}
+
+export default function Year({ year }: Props) {
 	return(
-		<div>
+		<div className={styles.about}>
 			<Head>
-				<title>About | The Tower</title>
+				<title>About the {year} staff | The Tower</title>
 				<meta
 					property="og:title"
 					content="About | The Tower"
@@ -15,7 +34,6 @@ export default function Subscribe() {
 				/>
 			</Head>
 			<h1>About</h1>
-
 		</div>
-	);
+	)
 }
