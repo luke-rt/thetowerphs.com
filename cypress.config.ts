9159fd43b4bf3bb0321b32784467d727c80e9320
ltgr <luke.rtong@gmail.com>
2022-06-07 22:52:24 -0400

    bump cypress to v10

---
+++ cypress.config.ts
@@ -0,0 +1,25 @@
+/** @format */
+
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+	video: false,
+	projectId: "bsg889",
+
+	e2e: {
+		// We've imported your old cypress plugins here.
+		// You may want to clean this up later by importing these.
+		setupNodeEvents(on, config) {
+			return require("./cypress/plugins/index.js")(on, config);
+		},
+		baseUrl: "http://localhost:3000",
+		specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+	},
+
+	component: {
+		devServer: {
+			framework: "next",
+			bundler: "webpack",
+		},
+	},
+});
