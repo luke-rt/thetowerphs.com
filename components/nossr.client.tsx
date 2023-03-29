cc86534acfa1f33913680e4ca4851ea1084d0d6c
unknown <knosmos@gmail.com>
2023-03-28 21:36:52 -0400

    make typescript happy

--- components/nossr.client.tsx
+++ components/nossr.client.tsx
@@ -3,7 +3,17 @@
 import dynamic from "next/dynamic";
 import React from "react";
 
-const NoSSR = props => <React.Fragment>{props.children}</React.Fragment>;
+const NoSSR = (props: {
+	children:
+		| string
+		| number
+		| boolean
+		| React.ReactElement<any, string | React.JSXElementConstructor<any>>
+		| React.ReactFragment
+		| React.ReactPortal
+		| null
+		| undefined;
+}) => <React.Fragment>{props.children}</React.Fragment>;
 
 export default dynamic(() => Promise.resolve(NoSSR), {
 	ssr: false,
